2cb0b50fda6ad4a04b3245b3e4b80de9d39330c5
gFulvio <gfulvio.gaglio@gmail.com>
2018-08-08 10:45:32 +0200

    è corretto?

--- esercizi-condizioni/il-conta-cifre.js
+++ esercizi-condizioni/il-conta-cifre.js
@@ -4,5 +4,18 @@
   http://www.imparareaprogrammare.it
 */
 
-var number = '1234';
-console.log(number.length);
+var number = 1234;
+
+number = number.toString;
+
+x = number.length;
+
+if (x = 4){
+  console.log("4");
+} else if (x = 3){
+  console.log("3");
+} else if (x = 2){
+  console.log("2");
+} else if (x = 1){
+  console.log("1");
+}
