e9945c16019bcd44da871034780febbc00989e16
gFulvio <42178224+gFulvio@users.noreply.github.com>
2018-08-16 17:15:05 +0200

    incompleto, difficoltà

--- esercizi-condizioni/chi-la-azzecca.js
+++ esercizi-condizioni/chi-la-azzecca.js
@@ -5,3 +5,20 @@
   Hint: Per generare un numero casuale utlizza la funzione javascript random, che restituisce un intervallo compreso tra 0 e 1 che necessita di essere convertito per il tuo intervallo.
   http://www.imparareaprogrammare.it
 */
+
+function getRandomIntInclusive(1, 100) {
+  1 = Math.ceil(1);
+  100 = Math.floor(100);
+  return Math.floor(Math.random() * (100 - 1 + 1)) + 1;
+}
+
+var player1 = 2;
+var player2 = 35;
+
+if (player1 = math.random){
+  console.log("Player1 ha azzeccato!");
+} else if (player2 = math.random){
+  console.log("Player2 ha azzeccato!");
+} else {
+  console.log("Avete sbagliato, il numero esatto è " + )
+}
