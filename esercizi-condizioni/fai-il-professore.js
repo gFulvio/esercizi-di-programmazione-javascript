05a6fdc790b06362d289b3156c5511628da05625
gFulvio <gfulvio.gaglio@gmail.com>
2018-08-27 17:02:01 +0200

    Completato

--- esercizi-condizioni/fai-il-professore.js
+++ esercizi-condizioni/fai-il-professore.js
@@ -13,3 +13,40 @@
   Realizzare una versione con il costrutto Switch-Case.
   http://www.imparareaprogrammare.it
 */
+
+var v = 22;
+
+/*if (v < 18){
+  console.log("insufficiente");
+} else if (18 <= v && v < 21){
+  console.log("sufficiente");
+} else if (21 <= v && v < 24){
+  console.log("buono");
+} else if (24 <= v && v < 27){
+  console.log("distinto");
+} else if (27 <= v && v < 29){
+  console.log("ottimo");
+} else if (v == 30){
+  console.log("eccellente");
+}*/
+
+switch (true){
+  case (v < 18):
+    console.log("insufficiente");
+    break;
+  case (v >= 18) && (v < 21):
+    console.log("sufficiente");
+    break;
+  case (v >= 21) && (v < 24):
+    console.log("buono");
+    break;
+  case (v >= 24) && (v < 27):
+    console.log("distinto");
+    break;
+  case (v >= 27) && (v <= 29):
+    console.log("ottimo");
+    break;
+  case (v == 30):
+    console.log("eccellente");
+    break;
+}
