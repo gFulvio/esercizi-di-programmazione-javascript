c606e283adb9d89efa0e37a2ffd1ae2cd97317d9
gFulvio <gfulvio.gaglio@gmail.com>
2018-09-12 12:45:49 +0200

    Completi

--- esercizi-array/operazioni-tra-array.js
+++ esercizi-array/operazioni-tra-array.js
@@ -10,3 +10,45 @@
   Hint: Per generare un numero casuale utlizza la funzione javascript random, che restituisce un intervallo compreso tra 0 e 1 che necessita di essere convertito per il tuo intervallo.
   http://www.imparareaprogrammare.it
 */
+
+//Definizione degli array con numeri casuali;
+var min = 1;
+var max = 10;
+
+var array1 = Array.from({length: 10}, (v, i) => Math.floor(Math.random() * (max - min + 1)) + min);
+console.log(array1);
+
+var array2 = Array.from({length: 10}, (v, i) => Math.floor(Math.random() * (max - min + 1)) + min);
+console.log(array2);
+
+//Dichiarazione e definizione variabili operazione e risultato
+var operazione = prompt("Inserisci un operatore aritmetico");
+var arrayOperazione = 0;
+
+//Applicazione operazione scelta
+switch (operazione){
+  case "+":
+    for (i = 0; i <= 10; i++){
+      arrayOperazione = Array.from({length: 10}, (v, i) => array1[i]+array2[i]);
+    };
+      console.log(`la somma tra array1 e array2 fa ${arrayOperazione}`);
+  break;
+  case "-":
+    for (i = 0; i <= 10; i++){
+      arrayOperazione = Array.from({length: 10}, (v, i) => array1[i]-array2[i]);
+    };
+    console.log(`la differenza tra array1 e array2 fa ${arrayOperazione}`);
+  break;
+  case "*":
+    for (i = 0; i <= 10; i++){
+      arrayOperazione = Array.from({length: 10}, (v, i) => array1[i]*array2[i]);
+    };
+    console.log(`la moltiplicazione tra array1 e array2 fa ${arrayOperazione}`);
+  break;
+  case "/":
+    for (i = 0; i <= 10; i++){
+      arrayOperazione = Array.from({length: 10}, (v, i) => array1[i]/array2[i]);
+    };
+    console.log(`la divisione tra array1 e array2 fa ${arrayOperazione}`);
+  break;
+  }
