a7c9432e8dc9f37e0d4b79f3a5c3c673b18838bd
gFulvio <gfulvio.gaglio@gmail.com>
2018-09-12 16:20:07 +0200

    Completato
    Risolto usando il metodo sort con la funzione compare

--- esercizi-array/metti-un-poco-di-ordine.js
+++ esercizi-array/metti-un-poco-di-ordine.js
@@ -6,3 +6,17 @@
   Prova ad ordinali in modo crescente.
   http://www.imparareaprogrammare.it
 */
+
+
+var array1 = [37, 55, 1, 92, 70, 12, 42, 28, 23, 32];
+array1.sort(function compareNumbers(a, b) {
+  return b - a;
+});
+console.log(array1);
+
+//Variante
+var array2 = [37, 55, 1, 92, 70, 12, 42, 28, 23, 32];
+array2.sort(function compareNumbers(a, b) {
+  return a - b;
+});
+console.log(array2);
