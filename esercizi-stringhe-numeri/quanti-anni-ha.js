9537c8a7a713f414d6e9b3bbb7e58d85f6044d0e
gFulvio <gfulvio.gaglio@gmail.com>
2018-09-12 12:46:16 +0200

    Completi

--- esercizi-stringhe-numeri/quanti-anni-ha.js
+++ esercizi-stringhe-numeri/quanti-anni-ha.js
@@ -3,3 +3,13 @@
   Scrivi un programma che prenda in input un anno di nascita e determini l'età della persona, calcola anche quanti anni sono necessari per raggiungere i 100. Stampa in output entrambi i risultati.
   http://www.imparareaprogrammare.it
 */
+
+var birth = 1963;
+var todayYear = 2018;
+var hundred = 100;
+
+var age = todayYear - birth;
+var toHundred = hundred - age;
+
+console.log(age);
+console.log(toHundred);
