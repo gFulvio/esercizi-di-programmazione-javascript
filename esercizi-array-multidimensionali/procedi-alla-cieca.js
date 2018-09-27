f4e6754cd505a28e5611f4cc92753afa8badaf19
gFulvio <gfulvio.gaglio@gmail.com>
2018-09-27 11:52:18 +0200

    Completo

--- esercizi-array-multidimensionali/procedi-alla-cieca.js
+++ esercizi-array-multidimensionali/procedi-alla-cieca.js
@@ -4,3 +4,14 @@
 
   http://www.imparareaprogrammare.it
 */
+
+var min = 0;
+var max = 100;
+
+var matrix = [];
+
+for (i = 0; i < 101; i++){
+  matrix[i] = Array.from({length: 100}, (v, i) => Math.floor(Math.random() * (max - min + 1)) + min);
+}
+
+console.log(matrix);
