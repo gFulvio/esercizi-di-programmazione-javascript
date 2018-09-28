53e5fecfefd41712a0315cf81f2ff87a38a121fe
gFulvio <gfulvio.gaglio@gmail.com>
2018-09-28 12:23:15 +0200

    Completato

--- esercizi-array-multidimensionali/sommali-tutti.js
+++ esercizi-array-multidimensionali/sommali-tutti.js
@@ -4,8 +4,8 @@
     - Prenda in input due numeri, N e M.
     - Generi una matrice di NxM popolata di valori casuali da 1 a 100.
     - Stampi la matrice e la somma di tutti i valori contenuti.
-  
-    Input : 
+
+    Input :
       2 3
     Output:
       [
@@ -16,3 +16,29 @@
 
   http://www.imparareaprogrammare.it
 */
+
+//variabili per definire i limiti dei numeri casuali
+var min = 1;
+var max = 100;
+
+//variabile per calcolare la somma finale
+var somma = 0;
+
+//inserimento input da parte dell'utente
+var inputN = Number(prompt("inserisci primo numero"));
+var inputM = Number(prompt("inserisci secondo numero"));
+
+//generazione e stampa della matrice
+var matriceNxM = Array.from({length: inputN}, (v, i) =>
+                    Array.from({length: inputM}, (v, i) =>
+                        Math.floor(Math.random() * (max - min + 1)) + min));
+
+console.log(matriceNxM);
+
+//calcolo e stampa della somma dei numeri
+for (i = 0; i < inputN; i++){
+  matriceNxM[i].forEach(function(numero){
+    somma += numero;
+  });
+}
+console.log(somma);
