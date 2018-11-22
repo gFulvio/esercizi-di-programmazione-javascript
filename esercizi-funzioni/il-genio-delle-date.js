4a893292201e6a4e410074ae1bcc0e72903f699c
gFulvio <gfulvio.gaglio@gmail.com>
2018-11-22 12:38:39 +0100

    risolto

--- esercizi-funzioni/il-genio-delle-date.js
+++ esercizi-funzioni/il-genio-delle-date.js
@@ -1,10 +1,22 @@
 /*
   Il genio delle date
   Scrivi una funzione che prenda in input due argomenti, il Giorno e il Mese. La funzione deve restituire il numero del giorno compreso tra 1 e 366.
-    Input : 5, 2 
+    Input : 5, 2
     Output:
             36
 
-  Hint: puoi definire un array con i giorni di ogni mese e ciclarlo.          
+  Hint: puoi definire un array con i giorni di ogni mese e ciclarlo.
   http://www.imparareaprogrammare.it
 */
+
+
+function giornoMese (giorno, mese){
+  arrayMesi = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+  somma = 0;
+  for (i = 0; i < mese - 1; i++){
+    somma += arrayMesi[i];
+  }
+  return somma + giorno;
+}
+
+console.log(giornoMese(5, 2));
--- esercizi-funzioni/la-funzione-che-sembra-difficile.js
+++ esercizi-funzioni/la-funzione-che-sembra-difficile.js
@@ -7,3 +7,17 @@
             4
   http://www.imparareaprogrammare.it
 */
+
+function dueNumeri (n, k){
+  arrayN = Array.from(n.toString());
+  arrayRid = [];
+  if (k > n.toString().length){
+    return 0;
+  }
+  for (i = 0; i < k; i++){
+    arrayRid = arrayN.pop();
+  }
+  return arrayRid;
+}
+
+console.log(dueNumeri(123456, 3));
